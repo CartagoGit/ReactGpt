e4393e9523344d45e3a480616daebce2cd245e2d
Cartago <cartago.relaxingcup@gmail.com>
2024-01-27 15:47:03 +0100

    Add react-markdown dependency and update OrthographyPage layout

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,3 +1,11 @@
 export const OrthographyPage = () => {
-	return <div>OrthographyPage</div>;
+	return (
+		<div className="chat-container">
+			<div className="chat-messages">
+				<div className="grid grid-cols-12 gap-y-2">
+					{/* Bienvenida */}
+				</div>
+			</div>
+		</div>
+	);
 };
