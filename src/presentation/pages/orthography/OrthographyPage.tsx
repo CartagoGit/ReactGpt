593ec1e7c515e99b3134f8b41ebce5fa65eb116d
Cartago <cartago.relaxingcup@gmail.com>
2024-01-27 17:09:29 +0100

    Add useState hook and message rendering logic to OrthographyPage

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { UserMessage } from "../../components/chat-bubbles/UserMessage";
 import {
     GptMessage,
@@ -5,21 +6,50 @@ import {
     TypingLoader,
 } from "../../components/index.components";
 
+interface IMessage {
+    text: string;
+    isGpt: boolean;
+}
+
+const initMessage: IMessage = {
+    text: "Hola, puedes escribir en español, y te ayudo con las correcciones",
+    isGpt: true,
+};
+
 export const OrthographyPage = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [messages, setMessages] = useState<IMessage[]>([initMessage]);
+
+    const handlePost = async (text: string) => {
+		if(isLoading) return;
+        setIsLoading(true);
+        setMessages((prev) => [...prev, { text, isGpt: false }]);
+
+		setIsLoading(false);
+    };
+
     return (
         <div className="chat-container">
             <div className="chat-messages">
                 <div className="grid grid-cols-12 gap-y-2">
                     {/* Bienvenida */}
-                    <GptMessage text="Hola, puedes escribir en español, y te ayudo con las correcciones" />
-                    <UserMessage text="Mi mensaje" />
 
-                    <TypingLoader className="fade-in" />
+                    {messages.map(({ isGpt, text }, index) => {
+                        if (isGpt) {
+                            return <GptMessage key={index} text={text} />;
+                        } else {
+                            return <UserMessage key={index} text={text} />;
+                        }
+                    })}
+                    {isLoading && (
+                        <div className="col-start-1 col-end-13 fade-in">
+                            <TypingLoader />
+                        </div>
+                    )}
                 </div>
             </div>
-
             <TextMessageBox
-                onSendMessage={(message) => console.log(message)}
+                onSendMessage={handlePost}
                 placeholder="Escribe el texto a corregir"
                 enableCorrections
             />
