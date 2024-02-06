8729077e6443a8315f313542b767b7e41630c9dc
Cartago <cartago.relaxingcup@gmail.com>
2024-02-06 17:52:26 +0100

    Agregada lógica para manejar errores en el caso de uso de ortografía

--- src/core/use-cases/orthography.use-case.ts
+++ src/core/use-cases/orthography.use-case.ts
@@ -1,8 +1,12 @@
-
-export const orthographyUseCase = (prompt: string) => {
-    try {
-        console.log(prompt)
-    }catch(error){
-
-    }
-}
+export const orthographyUseCase = async (prompt: string) => {
+  try {
+    console.log(prompt);
+  } catch (error) {
+    return {
+      ok: true,
+      accuracy: 0,
+      error: [],
+      message: "No se pudo realizar la correción ortográfica",
+    };
+  }
+};
