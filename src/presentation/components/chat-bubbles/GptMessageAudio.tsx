46db3e74458586e0662f6a3b455537d45590e5ad
Cartago <cartago.relaxingcup@gmail.com>
2024-02-15 00:03:43 +0100

    Eliminar importaciones y dependencias no utilizadas en GptMessageAudio.tsx

--- src/presentation/components/chat-bubbles/GptMessageAudio.tsx
+++ src/presentation/components/chat-bubbles/GptMessageAudio.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import { IMessage } from "../../../shared/interfaces/message.interface";
 import { ITextToAudioResponse } from "../../../shared/interfaces/responses.interface";
 
